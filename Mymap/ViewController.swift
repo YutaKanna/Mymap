9fe8b9284f8cb90593cb7eb38d2b628408cb295c
YutaKanna <kannayuta0629@gmail.com>
2019-01-13 19:05:44 +0900

    Initial Commit

---
+++ Mymap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mymap
+//
+//  Created by 漢那優太 on 2019/01/13.
+//  Copyright © 2019 Yuta Kanna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
