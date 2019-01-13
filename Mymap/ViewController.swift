4988a70c39dd59abd87251f4c20acef4dd39bce2
YutaKanna <kannayuta0629@gmail.com>
2019-01-13 19:18:58 +0900

    検索窓とマップパーツをプログラムと関連付ける

--- Mymap/ViewController.swift
+++ Mymap/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBOutlet weak var inputText: UITextField!
+    
+    @IBOutlet weak var dispMap: MKMapView!
+    
 }
 
